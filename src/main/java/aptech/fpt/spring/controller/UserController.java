b67fca610afea36f8e692850c1c57b44da1e8212
namlephuong95 <namlephuong95@gmail.com>
2020-09-25 01:10:43 +0700

    spring exam

--- src/main/java/aptech/fpt/spring/controller/UserController.java
+++ src/main/java/aptech/fpt/spring/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,31 +23,35 @@ public class UserController {
     public String index(Model model){
         List<User> users = (List<User>) userRepository.findAll();
         model.addAttribute("users",users);
+        model.addAttribute("userAdd",new User());
         return "index";
     }
 
-    @RequestMapping(path = "/user/add", method = RequestMethod.GET)
-    public String addUser(@ModelAttribute User u){
+//    @RequestMapping(path = "/user/add", method = RequestMethod.GET)
+//    public String addUser(Model model){
 //        model.addAttribute("user",new User());
-        return "addUser";
-    }
+//        return "addUser";
+//    }
 
     @RequestMapping(path = "/user/save",method = RequestMethod.POST)
-    public String save(User user){
+    public String save(@Valid User user){
         userRepository.save(user);
-        return "redirect:/";
+        return "redirect:/user/index";
     }
 
     @RequestMapping(path = "/user/edit",method = RequestMethod.GET)
     public String editUser(@RequestParam("id") Long userId, Model model){
         Optional<User> userEdit = userRepository.findById(userId);
-        userEdit.ifPresent(user -> model.addAttribute("user",user));
+        userEdit.ifPresent(user -> model.addAttribute("userEdit",user));
+
+        List<User> users = (List<User>) userRepository.findAll();
+        model.addAttribute("users",users);
         return "editUser";
     }
 
     @RequestMapping(path = "/user/delete",method = RequestMethod.DELETE)
     public String deleteUser(@RequestParam("id") Long userId, Model model){
         userRepository.deleteById(userId);
-        return "redirect:/";
+        return "redirect:/user/index";
     }
 }
--- src/main/java/aptech/fpt/spring/repository/UserRepository.java
+++ src/main/java/aptech/fpt/spring/repository/UserRepository.java
@@ -3,5 +3,8 @@ package aptech.fpt.spring.repository;
 import aptech.fpt.spring.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+    List<User> findDistinctByName(String name);
 }
